8d2c509b42e57b9f46f6456de115a9ad2f4be34f
dnyana <dnyaneshgawade903@gmail.com>
2021-08-06 18:53:03 +0530

    [Dnyanesh] add :Creating Stack

---
+++ StackAndQueue/Stack.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StackAndQueue
+{
+    public class Stack
+    {
+        internal Node head;
+        internal void Push(int data)
+        {
+            Node node = new Node(data);
+            if (this.head == null)
+            {
+                this.head = node;
+            }
+            else
+            {
+                Node temp = this.head;
+                head = node;
+                head.next = temp;
+            }
+            Console.WriteLine("{0} is inserted into stack", node.data);
+        }
+        internal void Display()
+        {
+            Node temp = this.head;
+            while (temp != null)
+            {
+                Console.Write(temp.data + " ");
+                temp = temp.next;
+            }
+            Console.WriteLine();
+        }
+    }
+}
