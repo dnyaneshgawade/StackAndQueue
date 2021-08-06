03db24c118b3b058d5faeeb5155c20f84c9fbf71
dnyana <dnyaneshgawade903@gmail.com>
2021-08-06 19:16:57 +0530

    [Dnyanesh] add :Creating Queue

--- StackAndQueue/Program.cs
+++ StackAndQueue/Program.cs
@@ -6,15 +6,11 @@ namespace StackAndQueue
     {
         static void Main(string[] args)
         {
-            Stack stack = new Stack();
-            stack.Push(56);
-            stack.Push(30);
-            stack.Push(70);
-            stack.Display();
-            stack.Peek();
-            stack.Pop();
-            Console.WriteLine("Stack after pop operation");
-            stack.Display();
+            Queue queue = new Queue();
+            queue.Enqueue(56);
+            queue.Enqueue(30);
+            queue.Enqueue(70);
+            queue.Display();
         }
     }
 }
